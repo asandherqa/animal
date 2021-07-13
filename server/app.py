a5f89b409c17493ef41e0992ea32c16fc66fb443
asandherqa <asandherqa@gmail.com>
2021-07-13 21:19:56 +0000

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,14 +1,34 @@
-
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 import requests
+from os import getenv
+from sqlalchemy import desc
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+
+db = SQLAlchemy(app)
+
+class Animals(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), nullable=False)
+    noise = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def home():
     animal = requests.get('http://animal_noises_api:5000/get_animal') 
     noise = requests.post('http://animal_noises_api:5000/get_noise', data=animal.text) 
-    return render_template('index.html', animal=animal.text, noise=noise.text)
+
+    last_five_animals = Animals.query.order_by(desc(Animals.id)).limit(5).all()
+    db.session.add(
+        Animals(
+            type = animal.text,
+            noise = noise.text
+        )
+    )
+    db.session.commit()
+
+    return render_template('index.html', animal=animal.text, noise=noise.text, last_five_animals=last_five_animals)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
