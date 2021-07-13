0b7871f3b075761a92dd92edf01c29c6fd92de95
asandherqa <asandherqa@gmail.com>
2021-07-13 15:06:41 +0000

    initial commit

--- animal_api/app.py
+++ animal_api/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+  
+from flask import Flask, request
+import random
 
 app = Flask(__name__)
 
-# animal generator route here
+@app.route('/get_animal', methods = ['GET'])
+def get_animal():
+    return random.choice(['cow', 'horse', 'duck'])
 
-# animal noise generator route here
+@app.route('/get_noise', methods = ['POST'])
+def get_noise():
+
+    noises = {
+        "cow" : "moooo", 
+        "horse" : "neigh", 
+        "duck" : "quack"
+    }
+    return noises[request.data.decode('utf-8')]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
